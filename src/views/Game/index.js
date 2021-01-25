2cb00f248fcc1989c9798c946f0d9f99595b605f
paulo70 <paulofrontend@gmail.com>
2021-01-25 19:32:36 -0300

    Add car to road

--- src/views/Game/index.js
+++ src/views/Game/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import Field from '../../components/Field'
+import Field from '../../components/Field' 
+
+import Car from '../../assets/images/car.png'
 
 import './game.scss'
 
@@ -8,6 +10,7 @@ const Game = () => {
   return (
     <div className='wrapper-game'>
       <div className='wrapper-game-content'>
+        <img src = {Car}  className='wrapper-game-content-img'/>
       </div>
     </div>
   )
