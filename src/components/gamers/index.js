12e9797e28a3e0d382c354fe1bba255a47451930
paulo70 <paulofrontend@gmail.com>
2021-01-26 21:29:13 -0300

    Add get list of nicknames gamers

--- src/components/gamers/index.js
+++ src/components/gamers/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './gamer.scss'
 
 const Gamers = (props) => {
+
+  const [ nickname, setNickName ] = useState([])
+  const [ loadNicknames, setLoadNickNames ] = useState(true)
+
+  useEffect(() => {
+
+    const getNicknames = () => {
+      const gamerDB = localStorage['gamer']
+      const listGamers = gamerDB ? JSON.parse(gamerDB) : []
+      setNickName(listGamers)
+      
+      console.log(listGamers, 'who')
+    }
+
+    if( loadNicknames ) {
+      getNicknames()
+      setLoadNickNames(false)
+    }
+    
+  },[loadNicknames])
+
   return (
     <div className='box-gamers'>
      <h2 className='box-gamers-title'>Welcome Gamers</h2>
